c73343ca51b052b0b23eb12e6e1c53bbc16d3f93
jerikchan <jerikchan@gmail.com>
2024-06-17 13:24:50 +0800

    feat: shadow hover

--- src/components/sections/Application.tsx
+++ src/components/sections/Application.tsx
@@ -26,62 +26,12 @@ export default function Testimonials() {
           {/* <p>See what our clients are saying about us.</p> */}
         </motion.div>
         <div className="grid grid-cols-1 gap-12 mt-6 lg:grid-cols-3">
-          <Testimonial
-            content="The best on the net! Software development saved my business. I use
-        Software development often. I am really satisfied with my Software
-        Development."
-            name="Trushit Vyas"
-            designation="Co-Founder"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/images/塑料.jpg"
-            text="塑料"
-          />
-          <Testimonial
-            content="Mobile application developer is exactly what out business has been lacing. I have gotten at least 50 times the value from mobile apllication."
-            name="Kishan H. Sheth"
-            designation="Founder & CEO"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/images/陶瓷2.jpg"
-            text="陶瓷"
-          />
-          <Testimonial
-            content="Logo Design is the next killer app. We've used logo design for the last five years. Best. Product. Ever! Definetly worth the investment"
-            name="Romit Gandhi"
-            designation="Founder"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/images/涂料.jpg"
-            text="涂料"
-          />
-          <Testimonial
-            content="Logo Design is the next killer app. We've used logo design for the last five years. Best. Product. Ever! Definetly worth the investment"
-            name="Romit Gandhi"
-            designation="Founder"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/images/橡胶.png"
-            text="橡胶"
-          />
-          <Testimonial
-            content="Logo Design is the next killer app. We've used logo design for the last five years. Best. Product. Ever! Definetly worth the investment"
-            name="Romit Gandhi"
-            designation="Founder"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/images/医药2.png"
-            text="医药"
-          />
-          <Testimonial
-            content="Logo Design is the next killer app. We've used logo design for the last five years. Best. Product. Ever! Definetly worth the investment"
-            name="Romit Gandhi"
-            designation="Founder"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/images/造纸.png"
-            text="造纸"
-          />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/images/塑料.jpg" text="塑料" />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/images/陶瓷2.jpg" text="陶瓷" />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/images/涂料.jpg" text="涂料" />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/images/橡胶.png" text="橡胶" />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/images/医药2.png" text="医药" />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/images/造纸.png" text="造纸" />
         </div>
       </div>
     </div>
--- src/components/sections/Report.tsx
+++ src/components/sections/Report.tsx
@@ -24,40 +24,10 @@ export default function Testimonials() {
           {/* <p>See what our clients are saying about us.</p> */}
         </motion.div>
         <div className="grid grid-cols-1 gap-12 mt-6 lg:grid-cols-4">
-          <Testimonial
-            content="The best on the net! Software development saved my business. I use
-        Software development often. I am really satisfied with my Software
-        Development."
-            name="Trushit Vyas"
-            designation="Co-Founder"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/reports/report-1.png"
-          />
-          <Testimonial
-            content="Mobile application developer is exactly what out business has been lacing. I have gotten at least 50 times the value from mobile apllication."
-            name="Kishan H. Sheth"
-            designation="Founder & CEO"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/reports/report-2.png"
-          />
-          <Testimonial
-            content="Logo Design is the next killer app. We've used logo design for the last five years. Best. Product. Ever! Definetly worth the investment"
-            name="Romit Gandhi"
-            designation="Founder"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/reports/report-3.png"
-          />
-          <Testimonial
-            content="Logo Design is the next killer app. We've used logo design for the last five years. Best. Product. Ever! Definetly worth the investment"
-            name="Romit Gandhi"
-            designation="Founder"
-            variants={cardAnimation}
-            animate={controls}
-            picture="/reports/report-4.png"
-          />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/reports/report-1.png" />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/reports/report-2.png" />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/reports/report-3.png" />
+          <Testimonial variants={cardAnimation} animate={controls} picture="/reports/report-4.png" />
         </div>
       </div>
     </div>
