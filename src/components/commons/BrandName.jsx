5e91c6d8daf0e76355af804bbb5ce6945ef8975a
jerikchan <jerikchan@gmail.com>
2024-06-18 18:51:30 +0800

    feat: update branch

--- src/components/commons/BrandName.jsx
+++ src/components/commons/BrandName.jsx
@@ -5,9 +5,9 @@ import { useTranslation } from 'react-i18next'
 function BrandName({ isFooter = false }) {
   const { t } = useTranslation()
   return (
-    <div className={`brand ${isFooter === true ? 'footer' : ''}`}>
-      {/* <span>{t('brand')}</span> */}
-      <img src="/logo-h.png" className="w-[200px]" />
+    <div className="flex flex-col items-center gap-2">
+      <div className="text-[rgba(68,_114,_196,_0.8)] [letter-spacing:0.3rem] text-[2rem] text-base">{t('brand')}</div>
+      <img src="/logo-h.png" className="w-[120px] md:w-[200px]" />
     </div>
   )
 }
